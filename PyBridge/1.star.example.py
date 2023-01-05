fe228d6a811200f531f475e7738983ce4b9055ff
saiksk939 <ranto93900@gmail.com>
2023-01-05 16:34:09 +0530

    changes

--- PyBridge/1.star.example.py
+++ PyBridge/1.star.example.py
@@ -1,8 +1,29 @@
 from turtle import *
+#tutrle square
+#bgcolor('black')
+#pencolor('yellow')
+#pensize(10)
+#for i in range(4):
+#    forward(100)
+#    right(90)
+#hideturtle()
+#done()
+
+#turtle triangle
+#bgcolor('black')
+#pencolor('yellow')
+#pensize(10)
+#for i in range(3):
+#    forward(200)
+#    right(120)
+#hideturtle()
+#done()
+
+#turtle star
 bgcolor('black')
 pencolor('yellow')
 pensize(10)
-clrs=['red','green','yellow','purple','blue']
+clrs=['red','yellow','white','blue','green']
 for i in range(5):
     pencolor(clrs[i])
     forward(200)
